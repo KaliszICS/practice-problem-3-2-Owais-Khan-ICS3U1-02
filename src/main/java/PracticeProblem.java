2f4988baffd4b2c1d1c6502e44d413f395a8e744
Owais-Khan-ICS3U1-02 <343318663@tdsb.ca>
2026-03-24 10:44:54 -0400

    Update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,46 @@
+/**
+	* Lesson: 3.2 If and Else
+	* Author: Owais Ali Khan
+	* Date Created: March 24, 2026
+	* Date Last Modified: March 24, 2026
+	*/
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
+	}
+
+	public static String isFive(int num) {
+		if (num==5) {
+			return "The number is Five"		
+		} else {
+			return "The number is not Five"
+		}
+	}
+
+	public static String positiveOrNegative(double num) {
+		if (num>0) {
+			return "Positive"		
+		} else {
+			return "Negative"
+		}
+	}
+
+	public static String highOrLow(int num) {
+		if (num>100) {
+			return "High"		
+		} else {
+			return "Low"
+		}
+	}
+
+	public static String isHello(String str) {
+		if (str.equals("Hello")) {
+			return "The word is Hello"
+		} else {
+			return "The word is not Hello"
+		}
 	}
 
 	
